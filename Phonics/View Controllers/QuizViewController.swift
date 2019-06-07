507d1314bb67524da5440360b0f25cc7a4e26193
rohanrk <rohanrk2009@gmail.com>
2019-06-07 18:11:47 -0400

    Change letter font so all letter labels appear properly

--- Phonics/View Controllers/QuizViewController.swift
+++ Phonics/View Controllers/QuizViewController.swift
@@ -247,7 +247,7 @@ class QuizViewController : InteractiveGrowViewController {
             if self.difficulty == .easyDifficulty {
                 // alphabet letters: only letter, no image
                 wordView.useLetter(selectedLetters[index])
-                iPad() ? wordView.letterLabelView.editLabelFont(font: UIFont.comicSans(size: 65)) : wordView.letterLabelView.editLabelFont(font: UIFont.comicSans(size: 90))
+                iPad() ? wordView.letterLabelView.editLabelFont(font: UIFont.comicSans(size: 90)) : wordView.letterLabelView.editLabelFont(font: UIFont.comicSans(size: 65))
                 
             } else {
                 // phonics: include image
